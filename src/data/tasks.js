e34788095db03191b66724ce04983f0edde670b4
seiyadev <axelchavezuwu@gmail.com>
2022-11-06 17:31:03 -0600

    0.0.1

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "My first task",
+    description: "Mi first task",
+  },
+  {
+    id: 1,
+    title: "My second task",
+    description: "Mi second task",
+  },
+  {
+    id: 2,
+    title: "My third task",
+    description: "Mi third task",
+  },
+];
